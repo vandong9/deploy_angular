fe3cf2fae2257d1e164623fa72bff3e38fac40df
dongha <vandong9@gmail.com>
2022-12-03 18:35:24 +0700

    update

---
+++ scripts.ca747294222a5c46.js
@@ -0,0 +1,1 @@
+function scrollBack(){let l=document.getElementById("horizontal-scroll-wrapper");l.scrollLeft-=50,console.log(l.scrollLeft)}function scrollNext(){let l=document.getElementById("horizontal-scroll-wrapper");l.scrollLeft+=50,console.log(l.scrollLeft)}
