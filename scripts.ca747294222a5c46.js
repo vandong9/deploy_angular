fe6eeff73850aa99e8536ca09382c9dc0de2fc00
dongha <vandong9@gmail.com>
2022-12-04 21:45:34 +0700

    clear

--- scripts.ca747294222a5c46.js
+++
@@ -1,1 +0,0 @@
-function scrollBack(){let l=document.getElementById("horizontal-scroll-wrapper");l.scrollLeft-=50,console.log(l.scrollLeft)}function scrollNext(){let l=document.getElementById("horizontal-scroll-wrapper");l.scrollLeft+=50,console.log(l.scrollLeft)}
